5665174d7a4e05ef6ecdbb10ea13468c6c74dbfd
Jason Posch <30759825+Technikhighknee@users.noreply.github.com>
2025-06-09 16:32:57 +0200

    Fix build paths and tsconfig

--- electron-main/main.ts
+++ electron-main/main.ts
@@ -17,7 +17,7 @@ function createWindow() {
     },
   });
 
-  mainWindow.loadFile(path.join(__dirname, '../dist/renderer/index.html'));
+  mainWindow.loadFile(path.join(__dirname, '../renderer/index.html'));
 }
 
 app.whenReady().then(() => {
